515713eb7a9adb6a82f3def6662c51560a9246aa
Aarav Mittal <137450929+a2105z@users.noreply.github.com>
2026-08-08 10:24:26 -0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen; 
+        // Key is the actual number and value is the index 
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i]; 
+
+            if (seen.count(complement) > 0) {
+                return {seen[complement], i}; 
+            } else {
+                seen[nums[i]] = i; 
+            }
+        }
+        return {}; 
+        
+    }
+};
